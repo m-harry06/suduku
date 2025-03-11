a03fb215d6e6f18989e1600e74ec2af9b5e93b90
GDCesar <155081528+GDCesar@users.noreply.github.com>
2025-03-11 12:17:43 +0100

    Update and rename résolution.h to resolution.h

---
+++ resolution.h
@@ -0,0 +1,8 @@
+#indef RESOLUTION_H_INCLUDED
+#define RESOLUTION_H_INCLUDED
+
+bool est_sauve(int grille[N][N], int ligne, int colonne, int num);
+bool resoudre_sudoku(int grille[N][N]);
+void afficher_grille(int grille[N][N]);
+
+#endef 
